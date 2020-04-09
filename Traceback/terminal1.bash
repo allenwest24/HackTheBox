63873f22d8a8a046ee4117cad6498b6e10e5a879
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-08 23:39:07 -0400

    Update terminal1.bash

--- Traceback/terminal1.bash
+++ Traceback/terminal1.bash
@@ -25,3 +25,54 @@ Service Info: OS: Linux; CPE: cpe:/o:linux:linux_kernel
 
 Service detection performed. Please report any incorrect results at https://nmap.org/submit/ .
 Nmap done: 1 IP address (1 host up) scanned in 8.08 seconds
+.
+.
+.
+// Went to 10.10.10.181
+// Checked out the source @ view-source:http://10.10.10.181/
+// Found the line: "<!--Some of the best web shells that you might need ;)-->"
+// Searched it and it led to a site with a list of php scripts.
+.
+.
+.
+allenwest1@debian:~$ nano shell.txt
+allenwest1@debian:~$ wfuzz -c -z file,'shell.txt' http://10.10.10.181/FUZZ
+
+Warning: Pycurl is not compiled against Openssl. Wfuzz might not work correctly when fuzzing SSL sites. Check Wfuzz's documentation for more information.
+
+********************************************************
+* Wfuzz 2.3.4 - The Web Fuzzer                         *
+********************************************************
+
+Target: http://10.10.10.181/FUZZ
+Total requests: 20
+
+==================================================================
+ID   Response   Lines      Word         Chars          Payload    
+==================================================================
+
+000001:  C=404      9 L       32 W          285 Ch        "alfa3.php"
+000002:  C=404      9 L       32 W          290 Ch        "alfav3.0.1.php"
+000003:  C=404      9 L       32 W          286 Ch        "andela.php"
+000004:  C=404      9 L       32 W          290 Ch        "bloodsecv4.php"
+000005:  C=404      9 L       32 W          282 Ch        "by.php"
+000006:  C=404      9 L       32 W          285 Ch        "c99ud.php"
+000011:  C=404      9 L       32 W          301 Ch        "obfuscated-punknopass.php"
+000012:  C=404      9 L       32 W          291 Ch        "punk-nopass.php"
+000013:  C=404      9 L       32 W          289 Ch        "punkholic.php"
+000014:  C=404      9 L       32 W          283 Ch        "r57.php"
+000016:  C=404      9 L       32 W          288 Ch        "wso2.8.5.php"
+000015:  C=200     58 L      100 W         1261 Ch        "smevk.php"
+000018:  C=404      9 L       32 W          280 Ch        "FREE"
+000019:  C=404      9 L       32 W          287 Ch        "INTERNETZZZ"
+000017:  C=404      9 L       32 W          280 Ch        "Xh4H"
+000020:  C=200     44 L      151 W         1113 Ch        ""
+000007:  C=404      9 L       32 W          283 Ch        "cmd.php"
+000009:  C=404      9 L       32 W          288 Ch        "jspshell.jsp"
+000008:  C=404      9 L       32 W          299 Ch        "configkillerionkros.php"
+000010:  C=404      9 L       32 W          284 Ch        "mini.php"
+
+Total time: 2.085044
+Processed Requests: 20
+Filtered Requests: 0
+Requests/sec.: 9.592121
