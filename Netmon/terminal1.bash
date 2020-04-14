cbf40e21bd2fe74165921b9fa10ca978fd42a6c1
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-13 20:00:21 -0400

    Update terminal1.bash

--- Netmon/terminal1.bash
+++ Netmon/terminal1.bash
@@ -50,3 +50,81 @@ Host script results:
 
 Service detection performed. Please report any incorrect results at https://nmap.org/submit/ .
 Nmap done: 1 IP address (1 host up) scanned in 16.08 seconds
+allenwest1@debian:~$ ftp 10.10.10.152
+bash: ftp: command not found
+allenwest1@debian:~$ sudo apt-get install ftp
+[sudo] password for allenwest1: 
+Reading package lists... Done
+Building dependency tree       
+Reading state information... Done
+The following NEW packages will be installed:
+  ftp
+0 upgraded, 1 newly installed, 0 to remove and 155 not upgraded.
+Need to get 59.4 kB of archives.
+After this operation, 140 kB of additional disk space will be used.
+Get:1 http://deb.debian.org/debian buster/main amd64 ftp amd64 0.17-34.1 [59.4 kB]
+Fetched 59.4 kB in 0s (148 kB/s)
+Selecting previously unselected package ftp.
+(Reading database ... 246875 files and directories currently installed.)
+Preparing to unpack .../ftp_0.17-34.1_amd64.deb ...
+Unpacking ftp (0.17-34.1) ...
+Setting up ftp (0.17-34.1) ...
+update-alternatives: using /usr/bin/netkit-ftp to provide /usr/bin/ftp (ftp) in auto mode
+Processing triggers for man-db (2.8.5-2) ...
+allenwest1@debian:~$ ftp 10.10.10.152
+Connected to 10.10.10.152.
+220 Microsoft FTP Service
+Name (10.10.10.152:allenwest1): Anonymous
+331 Anonymous access allowed, send identity (e-mail name) as password.
+Password:
+230 User logged in.
+Remote system type is Windows_NT.
+ftp> ls
+200 PORT command successful.
+125 Data connection already open; Transfer starting.
+02-03-19  12:18AM                 1024 .rnd
+02-25-19  10:15PM       <DIR>          inetpub
+07-16-16  09:18AM       <DIR>          PerfLogs
+02-25-19  10:56PM       <DIR>          Program Files
+02-03-19  12:28AM       <DIR>          Program Files (x86)
+02-03-19  08:08AM       <DIR>          Users
+04-13-20  04:01PM       <DIR>          Windows
+226 Transfer complete.
+ftp> cat
+?Invalid command
+ftp> cd Users
+250 CWD command successful.
+ftp> ls
+200 PORT command successful.
+125 Data connection already open; Transfer starting.
+02-25-19  11:44PM       <DIR>          Administrator
+04-13-20  03:28PM       <DIR>          Public
+226 Transfer complete.
+ftp> cd Public
+250 CWD command successful.
+ftp> ls
+200 PORT command successful.
+125 Data connection already open; Transfer starting.
+02-03-19  08:05AM       <DIR>          Documents
+07-16-16  09:18AM       <DIR>          Downloads
+07-16-16  09:18AM       <DIR>          Music
+07-16-16  09:18AM       <DIR>          Pictures
+04-13-20  03:31PM                   84 tester.txt
+02-03-19  12:35AM                   33 user.txt
+07-16-16  09:18AM       <DIR>          Videos
+226 Transfer complete.
+ftp> get user.txt
+local: user.txt remote: user.txt
+200 PORT command successful.
+125 Data connection already open; Transfer starting.
+WARNING! 1 bare linefeeds received in ASCII mode
+File may not have transferred correctly.
+226 Transfer complete.
+33 bytes received in 0.02 secs (1.4278 kB/s)
+.
+.
+.
+// Now went to terminal 2 to retreive user.txt on my ~/ dir
+.
+.
+.
