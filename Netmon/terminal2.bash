a8eb18b7caf61ddb0f54830172796a9a3d8a6fe2
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-13 20:01:32 -0400

    Create terminal2.bash

---
+++ Netmon/terminal2.bash
@@ -0,0 +1,18 @@
+allenwest1@debian:~$ cd Desktop/
+allenwest1@debian:~/Desktop$ ls
+advent_of_cod         joanna.pen       Prog8.jar
+authorized_keys       msfinstall       Prog8RE.java
+computer.desktop      network.desktop  pwn3yb0i.ovpn
+EthicalHack           nmap_scan.gnmap  rockyou.txt
+exploitReference.txt  nmap_scan.nmap   shell.py
+hacktheboxbs          nmap_scan.xml    Tint
+home                  openNetAdmin.sh  trash-can.desktop
+id_rsa                prod.dtsConfig   user-home.desktop
+allenwest1@debian:~/Desktop$ cd ..
+allenwest1@debian:~$ ls
+10.10.10.162.gnmap  Desktop    m10x.war  shell.txt
+10.10.10.162.nmap   Documents  Music     Templates
+10.10.10.162.xml    Downloads  Pictures  user.txt
+db_injection.py     hash.txt   Public    Videos
+allenwest1@debian:~$ cat user.txt
+*********************************
