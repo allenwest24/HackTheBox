9e70cc0bb8ccbca2a94349f6c9e411ab4f751a28
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-15 16:22:44 -0400

    Create terminal2.bash

---
+++ Monteverde/terminal2.bash
@@ -0,0 +1,23 @@
+allenwest1@debian:~/Desktop$ cat azure.xml 
+��<Objs Version="1.1.0.1" xmlns="http://schemas.microsoft.com/powershell/2004/04">
+  <Obj RefId="0">
+    <TN RefId="0">
+      <T>Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential</T>
+      <T>System.Object</T>
+    </TN>
+    <ToString>Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential</ToString>
+    <Props>
+      <DT N="StartDate">2020-01-03T05:35:00.7562298-08:00</DT>
+      <DT N="EndDate">2054-01-03T05:35:00.7562298-08:00</DT>
+      <G N="KeyId">00000000-0000-0000-0000-000000000000</G>
+      <S N="Password">4n0therD4y@n0th3r$</S>
+    </Props>
+  </Obj>
+</Objs>
+.
+.
+.
+// mhope:4n0therD4y@n0th3r$
+.
+.
+.
