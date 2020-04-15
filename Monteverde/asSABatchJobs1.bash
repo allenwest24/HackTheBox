fb8a626a27a5182c37dca1801b7e7631a03f82de
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-15 15:54:14 -0400

    Create asSABatchJobs1.bash

---
+++ Monteverde/asSABatchJobs1.bash
@@ -0,0 +1,19 @@
+smb: \> ls
+  .                                   D        0  Fri Jan  3 08:12:48 2020
+  ..                                  D        0  Fri Jan  3 08:12:48 2020
+  dgalanos                            D        0  Fri Jan  3 08:12:30 2020
+  mhope                               D        0  Fri Jan  3 08:41:18 2020
+  roleary                             D        0  Fri Jan  3 08:10:30 2020
+  smorgan                             D        0  Fri Jan  3 08:10:24 2020
+
+                524031 blocks of size 4096. 519955 blocks available
+smb: \> cd mhope
+smb: \mhope\> ls
+  .                                   D        0  Fri Jan  3 08:41:18 2020
+  ..                                  D        0  Fri Jan  3 08:41:18 2020
+  azure.xml                          AR     1212  Fri Jan  3 08:40:23 2020
+
+                524031 blocks of size 4096. 519955 blocks available
+smb: \mhope\> get azure.xml
+getting file \mhope\azure.xml of size 1212 as azure.xml (11.4 KiloBytes/sec) (average 11.4 KiloBytes/sec)
+smb: \mhope\> exit
