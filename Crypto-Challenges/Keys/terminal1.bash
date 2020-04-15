124a7e3a1ffc83fa0ddcd8af80b04c7cc86ae4bf
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-15 12:49:08 -0400

    Create terminal1.bash

---
+++ Crypto-Challenges/Keys/terminal1.bash
@@ -0,0 +1,28 @@
+allenwest1@debian:~$ cd Downloads/
+allenwest1@debian:~/Downloads$ ls
+ apps                    HeapSort.java
+ BoardUtils.java         keys.zip
+'cat(1).ab'              LightEmAllExamples.java
+ cat.ab                  LightEmAll.java
+ Cat.zip                 LightEmAll.zip
+ ch02-assignment+hints   platform-tools
+ ch03_submit.tar.gz      platform-tools_r29.0.6-linux.zip
+ Edge.java               Queue.java
+ GamePieces.java         shared
+allenwest1@debian:~/Downloads$ unzip keys.zip 
+Archive:  keys.zip
+[keys.zip] keys.txt password: 
+  inflating: keys.txt                
+allenwest1@debian:~/Downloads$ cat keys.txt 
+hBU9lesroX_veFoHz-xUcaz4_ymH-D8p28IP_4rtjq0=
+gAAAAABaDDCRPXCPdGDcBKFqEFz9zvnaiLUbWHqxXqScTTYWfZJcz-WhH7rf_fYHo67zGzJAdkrwATuMptY-nJmU-eYG3HKLO9WDLmO27sex1-R85CZEFCU=
+allenwest1@debian:~/Downloads$ echo fernet
+fernet
+.
+.
+.
+// Determined to be fernet after some research 
+// Went to https://asecuritysite.com/encryption/ferdecode to decode.
+.
+.
+.
