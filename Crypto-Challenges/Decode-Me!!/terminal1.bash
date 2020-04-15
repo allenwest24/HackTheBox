6603e323c6b0e23baaeea665e65599b5fa4303df
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-15 13:13:10 -0400

    Create terminal1.bash

---
+++ Crypto-Challenges/Decode-Me!!/terminal1.bash
@@ -0,0 +1,41 @@
+allenwest1@debian:~/Downloads$ ls
+ apps                    HeapSort.java
+ BoardUtils.java         keys.txt
+'cat(1).ab'              keys.zip
+ cat.ab                  LightEmAllExamples.java
+ Cat.zip                 LightEmAll.java
+ ch02-assignment+hints   LightEmAll.zip
+ ch03_submit.tar.gz      platform-tools
+'Decode_Me!!.zip'        platform-tools_r29.0.6-linux.zip
+ Edge.java               Queue.java
+ GamePieces.java         shared
+allenwest1@debian:~/Downloads$ unzip 'Decode_Me!!.zip' 
+Archive:  Decode_Me!!.zip
+[Decode_Me!!.zip] Decode.txt password: 
+  inflating: Decode.txt              
+allenwest1@debian:~/Downloads$ ls
+ apps                    HeapSort.java
+ BoardUtils.java         keys.txt
+'cat(1).ab'              keys.zip
+ cat.ab                  LightEmAllExamples.java
+ Cat.zip                 LightEmAll.java
+ ch02-assignment+hints   LightEmAll.zip
+ ch03_submit.tar.gz      platform-tools
+'Decode_Me!!.zip'        platform-tools_r29.0.6-linux.zip
+ Decode.txt              Queue.java
+ Edge.java               shared
+ GamePieces.java
+allenwest1@debian:~/Downloads$ cat Decode.txt 
+993gmULBNujjrZCDev3W8kAVaLkXiyHhCL3500188bA=
+
+gAAAAABboRUb0FsuiYBk1tsXRDr6KAzU1xrNSUv7grB-G-dAEeyqj99kUebz466I2VcH5xDa5HEc5KkbgTklQ7tm9JCRPlJtRng1Ns3VEvbrk7B835OINfPnRbc
+-UIOnnCmW3CgMdMtf5wGLN299AZEzxIvuy71WC5d9xJDchyiORycuzCth95-4nTKphlNQQ2ko3DX72RxWeEjwt3mavnFXqcOCkGxUhJYmFltz_6ND56VGTrXZi_
+CK5xLODOX4sj1GNwN_CrU3sJ0obTdA2wF5OaDZLbA1GBPfK0PDlC9WxoUf85K0tFXKfqbt3c5YqtqfytNG5gTkbDFM2NjE7BveBf1DP9ca8g==
+.
+.
+.
+// Determined to be fernet
+// See decodeFernet.txt
+.
+.
+.
