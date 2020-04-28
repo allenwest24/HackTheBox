469f870c59ea9f7a984b5fbf36e5ac21edc6e560
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-04-28 13:03:29 -0400

    Create asNotch.bash

---
+++ Blocky/asNotch.bash
@@ -0,0 +1,17 @@
+allenwest1@debian:~/Desktop/blocky/com/myfirstplugin$ ssh notch@10.10.10.37
+The authenticity of host '10.10.10.37 (10.10.10.37)' can't be established.
+ECDSA key fingerprint is SHA256:lg0igJ5ScjVO6jNwCH/OmEjdeO2+fx+MQhV/ne2i900.
+Are you sure you want to continue connecting (yes/no)? yes
+Warning: Permanently added '10.10.10.37' (ECDSA) to the list of known hosts.
+notch@10.10.10.37's password: 
+Welcome to Ubuntu 16.04.2 LTS (GNU/Linux 4.4.0-62-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+7 packages can be updated.
+7 updates are security updates.
+
+
+Last login: Tue Jul 25 11:14:53 2017 from 10.10.14.230
