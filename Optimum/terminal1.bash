65a5b6aa424ee527410cfac4b48dd018dd55419b
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-05-16 15:07:03 -0400

    Update terminal1.bash

--- Optimum/terminal1.bash
+++ Optimum/terminal1.bash
@@ -70,4 +70,28 @@ allenwest1@debian:~/Desktop$ nano 39161.py
 .
 .
 .
+allenwest1@debian:~/Desktop$ ip a
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host 
+       valid_lft forever preferred_lft forever
+2: enp0s3: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
+    link/ether 08:00:27:65:f0:2f brd ff:ff:ff:ff:ff:ff
+    inet 10.0.2.15/24 brd 10.0.2.255 scope global enp0s3
+       valid_lft forever preferred_lft forever
+    inet6 fe80::a00:27ff:fe65:f02f/64 scope link 
+       valid_lft forever preferred_lft forever
+3: tun0: <POINTOPOINT,MULTICAST,NOARP,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UNKNOWN group default qlen 100
+    link/none 
+    inet 10.10.14.17/23 brd 10.10.15.255 scope global tun0
+       valid_lft forever preferred_lft forever
+    inet6 dead:beef:2::100f/64 scope global 
+       valid_lft forever preferred_lft forever
+    inet6 fe80::46f9:b4bc:4e03:5e39/64 scope link stable-privacy 
+       valid_lft forever preferred_lft forever
+allenwest1@debian:~/Desktop$ nano 39161.py 
+
+NOT FINISHED BECAUSE I CANT FIND MY NC.EXE :|
 
