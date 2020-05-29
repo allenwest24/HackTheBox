49b3f0877f7ab1fe68ded26c1998756a64fb4c49
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-05-29 16:26:06 -0400

    Update asHype.bash

--- Valentine/asHype.bash
+++ Valentine/asHype.bash
@@ -20,5 +20,72 @@ hype@Valentine:~/Desktop$ ls
 user.txt
 hype@Valentine:~/Desktop$ cat user.txt
 ********************************
+hype@Valentine:~$ ls -al
+total 144
+drwxr-xr-x 21 hype hype  4096 Feb  5  2018 .
+drwxr-xr-x  3 root root  4096 Dec 11  2017 ..
+-rw-------  1 hype hype   131 Feb 16  2018 .bash_history
+-rw-r--r--  1 hype hype   220 Dec 11  2017 .bash_logout
+-rw-r--r--  1 hype hype  3486 Dec 11  2017 .bashrc
+drwx------ 11 hype hype  4096 Dec 11  2017 .cache
+drwx------  9 hype hype  4096 Dec 11  2017 .config
+drwx------  3 hype hype  4096 Dec 11  2017 .dbus
+drwxr-xr-x  2 hype hype  4096 Dec 13  2017 Desktop
+-rw-r--r--  1 hype hype    26 Dec 11  2017 .dmrc
+drwxr-xr-x  2 hype hype  4096 Dec 11  2017 Documents
+drwxr-xr-x  2 hype hype  4096 Dec 11  2017 Downloads
+drwxr-xr-x  2 hype hype  4096 Dec 11  2017 .fontconfig
+drwx------  3 hype hype  4096 Dec 11  2017 .gconf
+drwx------  4 hype hype  4096 Dec 11  2017 .gnome2
+-rw-rw-r--  1 hype hype   132 Dec 11  2017 .gtk-bookmarks
+drwx------  2 hype hype  4096 Dec 11  2017 .gvfs
+-rw-------  1 hype hype   636 Dec 11  2017 .ICEauthority
+drwxr-xr-x  3 hype hype  4096 Dec 11  2017 .local
+drwx------  3 hype hype  4096 Dec 11  2017 .mission-control
+drwxr-xr-x  2 hype hype  4096 Dec 11  2017 Music
+drwxr-xr-x  2 hype hype  4096 Dec 11  2017 Pictures
+-rw-r--r--  1 hype hype   675 Dec 11  2017 .profile
+drwxr-xr-x  2 hype hype  4096 Dec 11  2017 Public
+drwx------  2 hype hype  4096 Dec 11  2017 .pulse
+-rw-------  1 hype hype   256 Dec 11  2017 .pulse-cookie
+drwx------  2 hype hype  4096 Dec 13  2017 .ssh
+drwxr-xr-x  2 hype hype  4096 Dec 11  2017 Templates
+-rw-r--r--  1 root root    39 Dec 13  2017 .tmux.conf
+drwxr-xr-x  2 hype hype  4096 Dec 11  2017 Videos
+-rw-------  1 hype hype     0 Dec 11  2017 .Xauthority
+-rw-------  1 hype hype 12173 Dec 11  2017 .xsession-errors
+-rw-------  1 hype hype  9659 Dec 11  2017 .xsession-errors.old
+hype@Valentine:~$ cat .bash_history 
+
+exit
+exot
+exit
+ls -la
+cd /
+ls -la
+cd .devs
+ls -la
+tmux -L dev_sess 
+tmux a -t dev_sess 
+tmux --help
+tmux -S /.devs/dev_sess 
+exit
+hype@Valentine:~$ cd /.devs/
+hype@Valentine:/.devs$ ls
+dev_sess
+hype@Valentine:/.devs$ tmux -x dev_sess 
+tmux: unknown option -- x
+usage: tmux [-28lquvV] [-c shell-command] [-f file] [-L socket-name]
+            [-S socket-path] [command [flags]]
+hype@Valentine:/.devs$ tmux -S dev_sess 
+[exited]
+.
+.
+.
+// This opened a session as root.
+// Continued in asRoot.bash
+.
+.
+.
 
 
