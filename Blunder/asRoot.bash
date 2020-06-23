64cba6579a5bc5591ab28a5482ec3b888fe449aa
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-23 11:50:45 -0400

    Create asRoot.bash

---
+++ Blunder/asRoot.bash
@@ -0,0 +1,14 @@
+root@blunder:/home/hugo# cd 
+cd 
+root@blunder:/# cd root
+cd root
+root@blunder:/root# ls
+ls
+root.txt
+root@blunder:/root# cat root.txt
+cat root.txt
+********************************
+.
+.
+.
+// Great success!!
