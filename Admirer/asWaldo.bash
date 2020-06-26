25cf9d5b4eafb0e672313b4e8562261472c62fe2
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-26 13:02:54 -0400

    Create asWaldo.bash

---
+++ Admirer/asWaldo.bash
@@ -0,0 +1,146 @@
+waldo@admirer:~$ sudo -l
+[sudo] password for waldo: 
+Matching Defaults entries for waldo on admirer:
+    env_reset, env_file=/etc/sudoenv, mail_badpass,
+    secure_path=/usr/local/sbin\:/usr/local/bin\:/usr/sbin\:/usr/bin\:/sbin\:/bin,
+    listpw=always
+
+User waldo may run the following commands on admirer:
+    (ALL) SETENV: /opt/scripts/admin_tasks.sh
+.
+.
+.
+// Obviously gunna check that out
+.
+.
+.
+waldo@admirer:~$ cat /opt/scripts/admin_tasks.sh
+#!/bin/bash
+
+view_uptime()
+{
+    /usr/bin/uptime -p
+}
+
+view_users()
+{
+    /usr/bin/w
+}
+
+view_crontab()
+{
+    /usr/bin/crontab -l
+}
+
+backup_passwd()
+{
+    if [ "$EUID" -eq 0 ]
+    then
+        echo "Backing up /etc/passwd to /var/backups/passwd.bak..."
+        /bin/cp /etc/passwd /var/backups/passwd.bak
+        /bin/chown root:root /var/backups/passwd.bak
+        /bin/chmod 600 /var/backups/passwd.bak
+        echo "Done."
+    else
+        echo "Insufficient privileges to perform the selected operation."
+    fi
+}
+
+backup_shadow()
+{
+    if [ "$EUID" -eq 0 ]
+    then
+        echo "Backing up /etc/shadow to /var/backups/shadow.bak..."
+        /bin/cp /etc/shadow /var/backups/shadow.bak
+        /bin/chown root:shadow /var/backups/shadow.bak
+        /bin/chmod 600 /var/backups/shadow.bak
+        echo "Done."
+    else
+        echo "Insufficient privileges to perform the selected operation."
+    fi
+}
+
+backup_web()
+{
+    if [ "$EUID" -eq 0 ]
+    then
+        echo "Running backup script in the background, it might take a while..."
+        /opt/scripts/backup.py &
+    else
+        echo "Insufficient privileges to perform the selected operation."
+    fi
+}
+
+backup_db()
+{
+    if [ "$EUID" -eq 0 ]
+    then
+        echo "Running mysqldump in the background, it may take a while..."
+        #/usr/bin/mysqldump -u root admirerdb > /srv/ftp/dump.sql &
+        /usr/bin/mysqldump -u root admirerdb > /var/backups/dump.sql &
+    else
+        echo "Insufficient privileges to perform the selected operation."
+    fi
+}
+
+
+
+# Non-interactive way, to be used by the web interface
+if [ $# -eq 1 ]
+then
+    option=$1
+    case $option in
+        1) view_uptime ;;
+        2) view_users ;;
+        3) view_crontab ;;
+        4) backup_passwd ;;
+        5) backup_shadow ;;
+        6) backup_web ;;
+        7) backup_db ;;
+
+        *) echo "Unknown option." >&2
+    esac
+
+    exit 0
+fi
+
+
+# Interactive way, to be called from the command line
+options=("View system uptime"
+         "View logged in users"
+         "View crontab"
+         "Backup passwd file"
+         "Backup shadow file"
+         "Backup web data"
+         "Backup DB"
+         "Quit")
+
+echo
+echo "[[[ System Administration Menu ]]]"
+PS3="Choose an option: "
+COLUMNS=11
+select opt in "${options[@]}"; do
+    case $REPLY in
+        1) view_uptime ; break ;;
+        2) view_users ; break ;;
+        3) view_crontab ; break ;;
+        4) backup_passwd ; break ;;
+        5) backup_shadow ; break ;;
+        6) backup_web ; break ;;
+        7) backup_db ; break ;;
+        8) echo "Bye!" ; break ;;
+
+        *) echo "Unknown option." >&2
+    esac
+done
+
+exit 0
+.
+.
+.
+// Call to a script called backup.py
+// Now shit gets complicated.
+// going to be using a netcat on terminal2 and make a shutil.py to get the system to call backup.py
+.
+.
+.
