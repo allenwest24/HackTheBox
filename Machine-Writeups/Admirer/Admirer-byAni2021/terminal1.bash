76db43dafca59fdbe6e4808f841acb1fdcf0983c
ani2021 <55056056+ani2021@users.noreply.github.com>
2020-07-06 13:50:06 -0400

    Update terminal1.bash

--- Machine-Writeups/Admirer/Admirer-byAni2021/terminal1.bash
+++ Machine-Writeups/Admirer/Admirer-byAni2021/terminal1.bash
@@ -1,1 +1,10 @@
 
+anirudh@kali:~$ nmap 10.10.10.187
+Starting Nmap 7.80 ( https://nmap.org ) at 2020-07-06 13:49 EDT
+Nmap scan report for 10.10.10.187
+Host is up (0.020s latency).
+Not shown: 997 closed ports
+PORT   STATE SERVICE
+21/tcp open  ftp
+22/tcp open  ssh
+80/tcp open  http
