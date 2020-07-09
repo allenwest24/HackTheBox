e5d19c37f2a99a97ab180d2dfc07e146a9f1dae5
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-07-08 23:56:14 -0400

    Create asSmith.bash

---
+++ Machine-Writeups/Cascade/asSmith.bash
@@ -0,0 +1,35 @@
+*Evil-WinRM* PS C:\Users\s.smith\Documents> cd ..
+*Evil-WinRM* PS C:\Users\s.smith> ls
+
+
+    Directory: C:\Users\s.smith
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+d-r---        3/25/2020  11:17 AM                Desktop
+d-r---        1/13/2020   3:36 AM                Documents
+d-r---        7/14/2009   3:34 AM                Downloads
+d-r---        7/14/2009   3:34 AM                Favorites
+d-r---        7/14/2009   3:34 AM                Links
+d-r---        7/14/2009   3:34 AM                Music
+d-r---        7/14/2009   3:34 AM                Pictures
+d-----        7/14/2009   3:34 AM                Saved Games
+d-r---        7/14/2009   3:34 AM                Videos
+
+
+*Evil-WinRM* PS C:\Users\s.smith> cd Desktop
+*Evil-WinRM* PS C:\Users\s.smith\Desktop> ls
+
+
+    Directory: C:\Users\s.smith\Desktop
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+-ar---         7/9/2020   4:27 AM             34 user.txt
+-a----        3/25/2020  11:17 AM           1031 WinDirStat.lnk
+
+
+*Evil-WinRM* PS C:\Users\s.smith\Desktop> cat user.txt
+********************************
