a839bec885078b528fbc5fa3650743cd58edc865
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-07-09 18:31:03 -0400

    Create asAdministrator.bash

---
+++ Machine-Writeups/Cascade/asAdministrator.bash
@@ -0,0 +1,39 @@
+*Evil-WinRM* PS C:\Users\Administrator\Documents> dir
+*Evil-WinRM* PS C:\Users\Administrator\Documents> cd ..
+*Evil-WinRM* PS C:\Users\Administrator> dir
+
+
+    Directory: C:\Users\Administrator
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+d-r---        1/26/2020  11:56 PM                Contacts
+d-r---        3/25/2020  11:17 AM                Desktop
+d-r---        1/28/2020   6:26 PM                Documents
+d-r---        1/26/2020  11:56 PM                Downloads
+d-r---        1/26/2020  11:56 PM                Favorites
+d-r---        1/26/2020  11:56 PM                Links
+d-r---        1/26/2020  11:56 PM                Music
+d-r---        1/26/2020  11:56 PM                Pictures
+d-r---        1/26/2020  11:56 PM                Saved Games
+d-r---        1/26/2020  11:56 PM                Searches
+d-r---        1/26/2020  11:56 PM                Videos
+-a----        3/25/2020  11:17 AM         645729 wds_current_setup.exe
+
+
+*Evil-WinRM* PS C:\Users\Administrator> cd Desktop
+*Evil-WinRM* PS C:\Users\Administrator\Desktop> dir
+
+
+    Directory: C:\Users\Administrator\Desktop
+
+
+Mode                LastWriteTime         Length Name
+----                -------------         ------ ----
+-ar---         7/9/2020   4:27 AM             34 root.txt
+-a----        3/25/2020  11:17 AM           1031 WinDirStat.lnk
+
+
+*Evil-WinRM* PS C:\Users\Administrator\Desktop> cat root.txt
+********************************
