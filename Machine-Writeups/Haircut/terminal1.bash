3c83be3660bf7578c656c55de8d906669b8e8786
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-10-12 14:28:58 -0400

    Update terminal1.bash

--- Machine-Writeups/Haircut/terminal1.bash
+++ Machine-Writeups/Haircut/terminal1.bash
@@ -2,27 +2,26 @@
 .
 .
 .
-allenwest1@debian:~$ cd Desktop/lazEnum/
-allenwest1@debian:~/Desktop/lazEnum$ python lazEnum.py 10.10.10.24
+kali@kali:~/Desktop/tools$ python ./lazEnum.py 10.10.10.24
 ** server can't find 24.10.10.10.in-addr.arpa: NXDOMAIN
 
 
 Welcome to lazEnum! Let's start with a ping check to see if this shit is even reachable first yeh?
 
 PING 10.10.10.24 (10.10.10.24) 56(84) bytes of data.
-64 bytes from 10.10.10.24: icmp_seq=1 ttl=63 time=23.2 ms
-64 bytes from 10.10.10.24: icmp_seq=2 ttl=63 time=18.5 ms
-64 bytes from 10.10.10.24: icmp_seq=3 ttl=63 time=23.6 ms
+64 bytes from 10.10.10.24: icmp_seq=1 ttl=63 time=20.5 ms
+64 bytes from 10.10.10.24: icmp_seq=2 ttl=63 time=21.4 ms
+64 bytes from 10.10.10.24: icmp_seq=3 ttl=63 time=21.5 ms
 
 --- 10.10.10.24 ping statistics ---
-3 packets transmitted, 3 received, 0% packet loss, time 5ms
-rtt min/avg/max/mdev = 18.540/21.771/23.554/2.292 ms
+3 packets transmitted, 3 received, 0% packet loss, time 2003ms
+rtt min/avg/max/mdev = 20.459/21.139/21.529/0.482 ms
 
 Sick! The server at that IP is up and running. Let's do a port scan. This part may take ages. Trust the system..
 
-Starting Nmap 7.70 ( https://nmap.org ) at 2020-10-06 20:11 EDT
+Starting Nmap 7.80 ( https://nmap.org ) at 2020-10-12 14:17 EDT
 Nmap scan report for 10.10.10.24
-Host is up (0.025s latency).
+Host is up (0.024s latency).
 Not shown: 998 closed ports
 PORT   STATE SERVICE VERSION
 22/tcp open  ssh     OpenSSH 7.2p2 Ubuntu 4ubuntu2.2 (Ubuntu Linux; protocol 2.0)
@@ -36,28 +35,28 @@ PORT   STATE SERVICE VERSION
 Service Info: OS: Linux; CPE: cpe:/o:linux:linux_kernel
 
 Service detection performed. Please report any incorrect results at https://nmap.org/submit/ .
-Nmap done: 1 IP address (1 host up) scanned in 8.07 seconds
+Nmap done: 1 IP address (1 host up) scanned in 8.36 seconds
 
 Since port 80 is open, lets do a dictionary attack to see if any subdirectories are accessible!
-
-=====================================================
-Gobuster v2.0.1              OJ Reeves (@TheColonial)
-=====================================================
-[+] Mode         : dir
-[+] Url/Domain   : http://10.10.10.24/
-[+] Threads      : 20
-[+] Wordlist     : ./resources/directory-list-2.3-medium.txt
-[+] Status codes : 200,204,301,302,307,403
-[+] Extensions   : php
-[+] Timeout      : 10s
-=====================================================
-2020/10/06 20:11:59 Starting gobuster
-=====================================================
+===============================================================
+Gobuster v3.0.1
+by OJ Reeves (@TheColonial) & Christian Mehlmauer (@_FireFart_)
+===============================================================
+[+] Url:            http://10.10.10.24
+[+] Threads:        20
+[+] Wordlist:       /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt
+[+] Status codes:   200,204,301,302,307,401,403
+[+] User Agent:     gobuster/3.0.1
+[+] Extensions:     php
+[+] Timeout:        10s
+===============================================================
+2020/10/12 14:17:27 Starting gobuster
+===============================================================
 /uploads (Status: 301)
 /exposed.php (Status: 200)
-=====================================================
-2020/10/06 20:21:29 Finished
-=====================================================
+===============================================================
+2020/10/12 14:26:46 Finished
+===============================================================
 
 SSH is open on port 22 so be looking for usernames and passwords.
 Starting a test for 10.10.10.24
